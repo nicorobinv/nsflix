7a508e9c19ecd554d9c894ae4b81f2c6ec17a303
nicorobinv <nicorobinv@gmail.com>
2020-11-15 16:05:06 +0900

    done
    all done

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,9 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Router from "Components/Router";
+import GlobalStyles from "Components/GlobalStyles";
 
 class App extends Component {
-  render(){
-    return <><Router /></>;
+  render() {
+    return (
+      <>
+        <Router />
+        <GlobalStyles />
+      </>
+    );
   }
 }
 
